328420bd77c18983f3fb4e05e90566b11b22831d
poomchaio <poomchai.ta@gmail.com>
2024-12-13 21:47:26 +0800

    update style

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { streamText } from "@/src/app/chat/stream";
-import { AiChat, useAsStreamAdapter } from "@nlux/react";
+import { AiChat, AiChatUI, useAsStreamAdapter } from "@nlux/react";
 import "@nlux/themes/nova.css";
 
 export default function Chat() {
@@ -8,32 +8,35 @@ export default function Chat() {
   const chatAdapter = useAsStreamAdapter(streamText);
 
   return (
-    <AiChat
-      adapter={chatAdapter}
-      personaOptions={{
-        assistant: {
-          name: "HarryBotter",
-          avatar:
-            "https://docs.nlkit.com/nlux/images/personas/harry-botter.png",
-          tagline: "Making Magic With Mirthful AI",
-        },
-        user: {
-          name: "Alex",
-          avatar: "https://docs.nlkit.com/nlux/images/personas/alex.png",
-        },
-      }}
-      conversationOptions={{
-        conversationStarters: [
-          // Funny prompts as if you're talking to HarryBotter
-          { prompt: "What is the spell to make my code work?" },
-          { prompt: "Can you show me a magic trick?" },
-          { prompt: "Where can I find the book of wizardry?" },
-        ],
-      }}
-      displayOptions={{
-        width: 600,
-        height: 400,
-      }}
-    />
+    <div className="flex justify-center w-screen h-screen items-center">
+      <AiChat
+        displayOptions={{ width: "80%", height: "50%" }}
+        adapter={chatAdapter}
+        personaOptions={{
+          assistant: {
+            name: "HarryBotter",
+            avatar:
+              "https://docs.nlkit.com/nlux/images/personas/harry-botter.png",
+            tagline: "Making Magic With Mirthful AI",
+          },
+          user: {
+            name: "Alex",
+            avatar: "https://docs.nlkit.com/nlux/images/personas/alex.png",
+          },
+        }}
+        // conversationOptions={{
+        //   conversationStarters: [
+        //     // Funny prompts as if you're talking to HarryBotter
+        //     { prompt: "What is the spell to make my code work?" },
+        //     { prompt: "Can you show me a magic trick?" },
+        //     { prompt: "Where can I find the book of wizardry?" },
+        //   ],
+        // }}
+      >
+        <AiChatUI.Greeting>
+          <span className="rounded">Hello! 👋</span>
+        </AiChatUI.Greeting>
+      </AiChat>
+    </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center  min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="grid grid-cols-[repeat(auto-fit,_minmax(200px,300px))] row-start-2 gap-4">
+    <div className="min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="grid grid-cols-[repeat(auto-fit,_minmax(200px,300px))] row-start-2 gap-4 justify-center">
         {Array.from({ length: 10 }).map((temp) => (
           <div className="border rounded-md overflow-hidden hover:scale-110 transition-all">
             <Link href="/chat">
@@ -16,53 +16,6 @@ export default function Home() {
           </div>
         ))}
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
